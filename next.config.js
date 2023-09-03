b4c9c5e376fc46d31b0fd004c9f106fa3f0acdd3
Joyce-Yuan <jyuan24@mit.edu>
2023-09-03 03:19:10 -0700

    deleted readme

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-}
+// const nextConfig = {
+//   experimental: {
+//     appDir: true,
+//   },
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+// module.exports = {
+//   basePath: '',
+// }
+
+const nextConfig = {
+  distDir: "build",
+};
+
+module.exports = nextConfig;
