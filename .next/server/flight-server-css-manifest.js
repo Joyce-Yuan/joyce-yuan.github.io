f153b330ed2a01ac36ecc53dbaede2fa05479286
Joyce-Yuan <jyuan24@mit.edu>
2023-09-03 01:38:46 -0700

    initial base website

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,10 @@
+self.__RSC_CSS_MANIFEST={
+  "__entry_css_mods__": {
+    "/Users/joyceyuan/Desktop/joyce-yuan.github.io/app/page": [
+      "/Users/joyceyuan/Desktop/joyce-yuan.github.io/styles/globals.css"
+    ]
+  },
+  "/Users/joyceyuan/Desktop/joyce-yuan.github.io/app/layout.tsx": [
+    "/Users/joyceyuan/Desktop/joyce-yuan.github.io/styles/globals.css"
+  ]
+}
